c10a2a24b36555727b570e80e9a4c1414021517c
Debasish Debnath <ddebnath241@gmail.com>
2025-10-07 18:58:51 +0530

    feat(auth): enhance sign-up and verification screens with improved UI and functionality

--- mobile/app/(auth)/sign-up.jsx
+++ mobile/app/(auth)/sign-up.jsx
@@ -1,8 +1,21 @@
-import { View, Text, Alert } from "react-native";
+import {
+  View,
+  Text,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import React, { useState } from "react";
 import { useRouter } from "expo-router";
 import { useSignUp } from "@clerk/clerk-expo";
-
+import { authStyles } from "../../assets/styles/auth.styles";
+import { COLORS } from "../../constants/colors";
+import { Ionicons } from "@expo/vector-icons";
+import verifyEmail from "./verify-email";
 const signUpScreen = () => {
   const router = useRouter();
   const { isLoaded, signUp } = useSignUp();
@@ -19,20 +32,114 @@ const signUpScreen = () => {
     if (password.length < 6) {
       return Alert.alert("Error", "password must be at least 6 characcters");
     }
-    if (!isLoaded) return;
-
-    setLoading(true);
+    if (!isLoaded) {
+      return;
+    } else {
+      setLoading(true);
+    }
 
     try {
-      
+      await signUp.create({ emailAddress: email, password });
+
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+
+      setPendingVerification(true);
     } catch (error) {
-      
+      Alert.alert(
+        "Error",
+        error.errors?.[0]?.message || "Failed to create account"
+      );
+      console.error(JSON.stringify(error, null, 2));
+    } finally {
+      setLoading(false);
     }
   };
 
+  if (pendingVerification)
+    return (
+      <verifyEmail email={email} onBack={() => setPendingVerification(false)} />
+    );
+
   return (
-    <View>
-      <Text>signUpScreen</Text>
+    <View style={authStyles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
+        style={authStyles.keyboardView}
+      >
+        <ScrollView
+          contentContainerStyle={authStyles.scrollContent}
+          showsVerticalScrollIndicator={false}
+        >
+          <View style={authStyles.imageContainer}>
+            <Image
+              source={require("../../assets/images/i2.png")}
+              style={authStyles.image}
+              contentFit="contain"
+            />
+          </View>
+
+          <Text style={authStyles.title}>Create Account</Text>
+          <View style={authStyles.formContainer}>
+            <View style={authStyles.inputContainer}>
+              <TextInput
+                style={authStyles.textInput}
+                placeholder="Enter email"
+                placeholderTextColor={COLORS.textLight}
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoComplete="none"
+              />
+            </View>
+            <View style={authStyles.inputContainer}>
+              <TextInput
+                style={authStyles.textInput}
+                placeholder="Enter password"
+                placeholderTextColor={COLORS.textLight}
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={!showPassword}
+                autoCapitalize="none"
+              />
+              <TouchableOpacity
+                style={authStyles.eyeButton}
+                onPress={() => setPassword(!showPassword)}
+              >
+                <Ionicons
+                  name={showPassword ? "eye-outline" : "eye-off-outline"}
+                  size={20}
+                  color={COLORS.textLight}
+                />
+              </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity
+              style={[
+                authStyles.authButton,
+                loading && authStyles.buttonDisabled,
+              ]}
+              onPress={handleSignUp}
+              disabled={loading}
+              activeOpacity={0.8}
+            >
+              <Text style={authStyles.buttonText}>
+                {loading ? "Creating Account..." : "Sign Up"}
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={authStyles.linkContainer}
+              onPress={() => router.back()}
+            >
+              <Text style={authStyles.linkText}>
+                Already have an account?{" "}
+                <Text style={authStyles.link}>Sign In</Text>
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </View>
   );
 };
--- mobile/app/(auth)/verify-email.jsx
+++ mobile/app/(auth)/verify-email.jsx
@@ -1,12 +1,61 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
+import { useSignUp } from "@clerk/clerk-expo";
+import { authStyles } from "../../assets/styles/auth.styles";
 
-const verifyEmailScreen = () => {
+const verifyEmail = ({ onBack, email }) => {
+  const { isLoaded, signUp, setactive } = useSignUp();
+  const [code, setCode] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleVerification = async () => {
+    if (!isLoaded) return;
+
+    setLoading(true);
+    try {
+      const signUpAttempt = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+
+      if (signUpAttempt.status === "complete") {
+        await setactive({ session: signUpAttempt.createdSessionId });
+      } else {
+        Alert.alert("Error", "Verification failed. Please try again later");
+        console.error(JSON.stringify(signUpAttempt, null, 2));
+      }
+    } catch (error) {
+      Alert.alert("Error", error.errors?.[0]?.message || "Verification failed");
+      console.error(JSON.stringify(signUpAttempt, null, 2));
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>verifyEmailScreen</Text>
+    <View style={authStyles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={authStyles.keyboardView}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
+      >
+        <ScrollView
+          contentContainerStyle={authStyles.scrollContent}
+          showsVerticalScrollIndicator={false}
+        >
+          <View style={authStyles.imageContainer}>
+            <Image />
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </View>
-  )
-}
+  );
+};
 
-export default verifyEmailScreen
+export default verifyEmail;
