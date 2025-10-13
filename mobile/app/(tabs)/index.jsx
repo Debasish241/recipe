00e25a5e8be8cbb3e82e6d825135d7c049688023
Debasish Debnath <ddebnath241@gmail.com>
2025-10-13 18:21:04 +0530

    feat(home): implement HomeScreen with data loading and category filtering functionality

--- mobile/app/(tabs)/index.jsx
+++ mobile/app/(tabs)/index.jsx
@@ -1,12 +1,143 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import {
+  View,
+  Text,
+  ScrollView,
+  RefreshControl,
+  TouchableOpacity,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "expo-router";
+import { MealAPI } from "../../services/mealAPI";
+import { homeStyles } from "../../assets/styles/home.styles";
+import { Image } from "expo-image";
 const HomeScreen = () => {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+  const router = useRouter();
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [recipes, setRecipes] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [featuredRecipe, setFeaturedRecipe] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
 
-export default HomeScreen
+  const loadData = async () => {
+    try {
+      setLoading(true);
+      const [apiCategories, randomMeals, featuredMeal] = await Promise.all([
+        MealAPI.getCategories(),
+        MealAPI.getRandomMeals(12),
+        MealAPI.getRandomMeal(),
+      ]);
+
+      const transformedCategories = apiCategories.map((cat, index) => ({
+        id: index + 1,
+        name: cat.strCategory,
+        image: cat.strCategoryThumb,
+        descripton: cat.strCategoryDescription,
+      }));
+      setCategories(transformedCategories);
+
+      const transformeddMeals = randomMeals
+        .map((meal) => MealAPI.transformMealData(meal))
+        .filter((meal) => meal !== null);
+
+      setRecipes(transformeddMeals);
+
+      const transformedFeatured = MealAPI.transformMealData(featuredMeal);
+      setFeaturedRecipe(transformedFeatured);
+    } catch (error) {
+      console.log("Error loading the data", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const loadCategoryData = async (category) => {
+    try {
+      const meals = await MealAPI.filterByCategory(category);
+      const transformeddMeals = meals
+        .map((meal) => MealAPI.transformMealData(meal))
+        .filter((meal) => meal !== null);
+
+      setRecipes(transformeddMeals);
+    } catch (error) {
+      console.error("Error loading category data:", error);
+      setRecipes([]);
+    }
+  };
+
+  const handleCategorySelect = async (category) => {
+    setSelectedCategory(category);
+    await loadCategoryData(category);
+  };
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    setSelectedCategory(null);
+    await loadData();
+    setRefreshing(false);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <View style={homeStyles.container}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        // refreshControl={() => {}}
+        contentContainerStyle={homeStyles.scrollContent}
+      >
+        <View style={homeStyles.welcomeSection}>
+          <Image
+            source={require("../../assets/images/lamb.png")}
+            style={{
+              width: 100,
+              height: 100,
+            }}
+          />
+          <Image
+            source={require("../../assets/images/chicken.png")}
+            style={{
+              width: 100,
+              height: 100,
+            }}
+          />
+          <Image
+            source={require("../../assets/images/pork.png")}
+            style={{
+              width: 100,
+              height: 100,
+            }}
+          />
+        </View>
+
+        {featuredRecipe && (
+          <View style={homeStyles.featuredSection}>
+            <TouchableOpacity
+              style={homeStyles.featuredSection}
+              activeOpacity={0.9}
+              onPress={() => router.push(`/recipe/${featuredRecipe.id}`)}
+            >
+              <View style={homeStyles.featuredImageContainer}>
+                <Image
+                  source={{ uri: featuredRecipe.image }}
+                  style={homeStyles.featuredImage}
+                  contentFit="cover"
+                  transition={500}
+                />
+                <View style={homeStyles.featuredOverlay}>
+                  <View style={homeStyles.featuredBadge}>
+                    <Text style={homeStyles.featuredBadgeText}>Featured</Text>
+                  </View>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default HomeScreen;
