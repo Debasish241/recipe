e6ed6c17678b2a247569f4f15415183b468bf8b4
Debasish Debnath <ddebnath241@gmail.com>
2025-10-13 15:22:13 +0530

    feat(tabs): implement tab navigation with Recipes, Search, and Favourites screens feat(layout): replace SafeAreaView with SafeScreen for improved layout handling

---
+++ mobile/app/(tabs)/favourites.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const favouritesScreen = () => {
+  return (
+    <View>
+      <Text>favouritesScreen</Text>
+    </View>
+  )
+}
+
+export default favouritesScreen
